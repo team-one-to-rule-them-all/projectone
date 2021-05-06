5c36a9e86a67d437702b58a4d45eecdd708eca47
Michael Rao <jmichaelrao@gmail.com>
2021-05-05 22:25:31 -0500

    Add pseudo code for js

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// init function to load page and remove hide class from intro section
+
+// event listeners for each button
+
+// function to surface loading screen (setInterval 3s, add hide to intro, remove hide from loading screen)
+
+// function to fetch LOTR quote
+
+// function to surface quote
+
+// function to surface final screen (add hide to loading screen, remove hide from final screen)
+
+// function to fetch cocktail name and ID e.g. i=gin by character selection
+
+// function to surface above data
+
+// function to fetch recipe using called ID from previous function
+
+// function to surface above data
+
+// function to reset
